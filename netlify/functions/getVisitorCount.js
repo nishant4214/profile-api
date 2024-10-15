f78489f9b5ef9bfd682678102d191477391ccc21
Nishant.Pande <Nishant.Pande@ESDS003568NP>
2024-10-15 17:41:58 +0530

    supabase added

--- netlify/functions/getVisitorCount.js
+++ netlify/functions/getVisitorCount.js
@@ -1,10 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
-const dotenv = require('dotenv');
 
-dotenv.config();
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseUrl = 'https://fgglnzwavwqttvodqwcd.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnZ2xuendhdndxdHR2b2Rxd2NkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg5ODkyNjYsImV4cCI6MjA0NDU2NTI2Nn0.-XnzbPZ_jG5HI_tfflNchpA-84EWoQaKESxCh5yijLo';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 exports.handler = async (req, res) => {
