ea35fbe92ad5281242506c8809427deabd12e97c
Nishant.Pande <Nishant.Pande@ESDS003568NP>
2024-10-15 18:54:17 +0530

    supabase added

--- netlify/functions/getVisitorCount.js
+++ netlify/functions/getVisitorCount.js
@@ -2,7 +2,7 @@ const { createClient } = require('@supabase/supabase-js');
 
 
 const supabaseUrl = 'https://fgglnzwavwqttvodqwcd.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnZ2xuendhdndxdHR2b2Rxd2NkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg5ODkyNjYsImV4cCI6MjA0NDU2NTI2Nn0.-XnzbPZ_jG5HI_tfflNchpA-84EWoQaKESxCh5yijLo';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnZ2xuendhdndxdHR2b2Rxd2NkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyODk4OTI2NiwiZXhwIjoyMDQ0NTY1MjY2fQ.a0eTNtw3dctakhMK1cIaoMZNGGHIhtnAq0tDGqRXARg';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 
