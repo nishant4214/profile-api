907f3b33dc7803cb109ecc60d2dbfc9663bb13dd
Nishant.Pande <Nishant.Pande@ESDS003568NP>
2024-10-15 17:43:35 +0530

    supabase added

--- netlify/functions/getVisitorCount.js
+++ netlify/functions/getVisitorCount.js
@@ -5,6 +5,22 @@ const supabaseUrl = 'https://fgglnzwavwqttvodqwcd.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnZ2xuendhdndxdHR2b2Rxd2NkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg5ODkyNjYsImV4cCI6MjA0NDU2NTI2Nn0.-XnzbPZ_jG5HI_tfflNchpA-84EWoQaKESxCh5yijLo';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-exports.handler = async (req, res) => {
-  // Your logic for retrieving visitor count
+
+exports.handler = async () => {
+  const { data, error } = await supabase
+    .from('visitor_count')
+    .select('count')
+    .single();
+
+  if (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error retrieving count' }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
 };
